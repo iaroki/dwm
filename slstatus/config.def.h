c2bd7e0c8d2b236abc47e5b3a58e1707f02b783b
iaroki <iaroki@protonmail.com>
2017-12-28 12:19:41 +0200

    update interval 30 sec

--- slstatus/config.def.h
+++ slstatus/config.def.h
@@ -1,7 +1,7 @@
 /* See LICENSE file for copyright and license details. */
 
 /* interval between updates (in ms) */
-static const int interval = 1000;
+static const int interval = 30000; // 30 sec
 
 /* text to show if no value can be retrieved */
 static const char unknown_str[] = "n/a";
