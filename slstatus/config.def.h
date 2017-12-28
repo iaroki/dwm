ebcde9f6d1b393bb8d005b83b16e06c5b3e3e096
iaroki <iaroki@protonmail.com>
2017-12-28 12:21:49 +0200

    compact

--- slstatus/config.def.h
+++ slstatus/config.def.h
@@ -52,8 +52,8 @@ static const char unknown_str[] = "n/a";
 static const struct arg args[] = {
 	/* function format          argument */
 //	{ cpu_perc,     "[CPU: %3s%%] ",    NULL                },
-	{ ram_used,     "[RAM: %sM] ",      NULL                },
-    { battery_perc, "[BAT: %s%%] ",     "BAT1"              },
+	{ ram_used,     "| RAM: %sM ",      NULL                },
+    { battery_perc, "BAT: %s%% ",     "BAT1"              },
 //    { wifi_essid,   "[WIFI: %s] ",      "wlp1s0"            },
-	{ datetime,     "[ %s ]",           "%a %d %b, %H:%M"   },
+	{ datetime,     "%s",           "%a %d %b, %H:%M"   },
 };
