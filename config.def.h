b154a0e9afa41aa0a9773e04cd372032ad495fa5
iaroki <iaroki@protonmail.com>
2018-01-15 13:01:15 +0200

    FM hotkey

--- config.def.h
+++ config.def.h
@@ -61,6 +61,7 @@ static const Layout layouts[] = {
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", normbgcolor, "-nf", normfgcolor, "-sb", selbgcolor, "-sf", selfgcolor, NULL };
 static const char *termcmd[]  = { "st", NULL };
+static const char *fmcmd[] = { "thunar", NULL };
 
 static Key keys[] = {
 	/* modifier                     key        function        argument */
@@ -68,6 +69,7 @@ static Key keys[] = {
 	{ MODALTKEY,                    XK_F2,     spawn,          {.v = dmenucmd } },
 	{ MODKEY|ShiftMask,             XK_Return, spawn,          {.v = termcmd } },
 	{ MODALTKEY,                    XK_F1,     spawn,          {.v = termcmd } },
+    { MODALTKEY,                    XK_F3,     spawn,          {.v = fmcmd } },
 	{ MODKEY,                       XK_b,      togglebar,      {0} },
 	{ MODKEY,                       XK_j,      focusstack,     {.i = +1 } },
 	{ MODALTKEY,                    XK_Tab,    focusstack,     {.i = +1 } },
