b03ff9b5bd1fea4b42f1553d78f6f246ae42f38f
iaroki <iaroki@protonmail.com>
2018-03-03 12:23:20 +0200

    improvements

--- build.sh
+++ build.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
 docker run --rm -v $PWD:/src iaroki/dwmbuild
+if [ ! -d "~/bin" ]; then
+    mkdir ~/bin
+fi
 mv -v bin/* ~/bin/
 rmdir -v bin
-echo "DWM BUILD DONE!"
+echo "==> DWM BUILD DONE!"
