a94ee8835b593433e12c05314f21ff094f4fe513
iaroki <iaroki@protonmail.com>
2018-03-26 10:30:58 +0300

    font size inc

--- st/config.def.h
+++ st/config.def.h
@@ -5,7 +5,7 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char font[] = "Monaco:size=10:antialias=true:autohint=true";
+static char font[] = "Monaco:size=12:antialias=true:autohint=true";
 static int borderpx = 2;
 #define histsize 2000
 
