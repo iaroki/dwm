db8c5f6b725f1596369495378fd462de55d9d962
iaroki <iaroki@protonmail.com>
2022-02-23 21:18:56 +0200

    set color theme to catppuccin

--- config.def.h
+++ config.def.h
@@ -12,15 +12,13 @@ static const int showbar            = 1;     /* 0 means no bar */
 static const int topbar             = 1;     /* 0 means bottom bar */
 static const char *fonts[]          = { "monospace:size=10" };
 static const char dmenufont[]       = "monospace:size=10";
-static const char col_gray1[]       = "#222222";
-static const char col_gray2[]       = "#444444";
-static const char col_gray3[]       = "#bbbbbb";
-static const char col_gray4[]       = "#eeeeee";
-static const char col_cyan[]        = "#333333";
+static const char col_bg[]          = "#1E1D2F";
+static const char col_gray[]        = "#6E6C7E";
+static const char col_white[]       = "#D9E0EE";
+static const char col_border[]      = "#96CDFB";
 static const char *colors[][3]      = {
-	/*               fg         bg         border   */
-	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
-	[SchemeSel]  = { col_gray4, col_cyan,  col_cyan  },
+[SchemeNorm]     = { col_gray, col_bg,  col_bg },
+[SchemeSel]      = { col_white, col_bg,  col_border  },
 };
 
 /* tagging */
